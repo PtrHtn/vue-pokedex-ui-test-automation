4f143209f456c3a95860ea9a6ecad560b050c04e
PtrHtn <ptrhtn@gmail.com>
2021-09-26 22:01:36 -0300

    Add cucumber HTML test reporter

--- src/test/java/test_runner/VuePokedexRunner.java
+++ src/test/java/test_runner/VuePokedexRunner.java
@@ -6,7 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty","json:target/cucumber-report/cucumber.json"},
+        plugin = {"pretty"
+                , "json:target/cucumber-report/cucumber-default-json-report.json"
+                , "html:target/cucumber-report/cucumber-default-html-report.html"
+                , "summary"
+        },
         features = {"classpath:feature_files"},
         glue = {"classpath:step_definitions", "helpers"},
         monochrome = false,
